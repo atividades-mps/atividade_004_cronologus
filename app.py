b35232f681eec51e8089e7b0991e41d3f66db786
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-01-12 10:50:29 -0400

    add database

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
 from src.api.controllers import AppController, UserController
+from src.api.repositories.database import init_db
 
 app = Flask(__name__)
 app.register_blueprint(AppController.blueprint)
 app.register_blueprint(UserController.blueprint)
+
+init_db(app)
